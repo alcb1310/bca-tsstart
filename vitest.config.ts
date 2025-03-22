1c96a15caa7d76e9944c74edf034282e765e0b2e
Andres Court <andres@andrescourt.com>
2025-03-22 11:04:10 -0500

    test(setup): setup global imports for testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,12 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'jsdom',
+    globals: true,
+    setupFiles: './tests/setup.ts',
   },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  }
 });
