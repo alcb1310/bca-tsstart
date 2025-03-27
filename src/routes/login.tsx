f5bb09ca2e66509ded7d72b2e9a934d47f53db72
Andres Court <andres@andrescourt.com>
2025-03-26 23:22:24 -0500

    feat(layout): center the form & add placeholder

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -4,7 +4,9 @@ import { z } from "zod";
 
 const schema = z.object({
 	email: z.string().email({ message: "Ingrese un correo válido" }),
-	password: z.string().min(3),
+	password: z
+		.string()
+		.min(3, { message: "La contraseña debe tener al menos 3 caracteres" }),
 });
 
 // type _formType = z.infer<typeof schema>;
@@ -30,20 +32,23 @@ function RouteComponent() {
 	});
 
 	return (
-		<div>
+		<div className="flex flex-row min-h-screen justify-center items-center">
 			<form
 				onSubmit={(e) => {
 					e.preventDefault();
 					e.stopPropagation();
 					form.handleSubmit();
 				}}
+				className="w-1/2 flex flex-col gap-4"
 			>
 				<form.AppField name="email">
-					{(field) => <field.TextField label="Email" />}
+					{(field) => <field.TextField label="Email" placeholder="Email" />}
 				</form.AppField>
 
 				<form.AppField name="password">
-					{(field) => <field.PasswordField label="Password" />}
+					{(field) => (
+						<field.PasswordField label="Password" placeholder="Contraseña" />
+					)}
 				</form.AppField>
 
 				<form.AppForm>
